72fd63e662ebfa9dcd14e13e51f3b51709e738be
김강연 <kangyeon9525@gmail.com>
2025-11-21 13:52:09 +0900

    feat - 메모프로젝트 기본 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import MemoContainer from './component/MemoContainer';
+import SideBar from './component/SideBar';
 
 function App() {
+  const [memos, setMemos] = useState([
+    {
+      title: 'Memo 1',
+      content: 'This is memo 1',
+      createdAt: 1763700036091, // 시간 값
+      updatedAt: 1763700068222, // 시간 값
+    },
+    {
+      title: 'Memo 2',
+      content: 'This is memo 2',
+      createdAt: 1763700136091, // 시간 값
+      updatedAt: 1763700168222, // 시간 값
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SideBar memos={memos} />
+      <MemoContainer />
     </div>
   );
 }
